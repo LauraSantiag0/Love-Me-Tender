532d36f94387f3a74db072b98146baca62e9251a
aishaathmanlali <aishaathmanal@gmail.com>
2024-07-30 10:40:18 +0000

    fix a syntax error

--- database/migrations/sqls/20240730102034-create-session-table-up.sql
+++ database/migrations/sqls/20240730102034-create-session-table-up.sql
@@ -2,6 +2,6 @@ CREATE TABLE session (
     token VARCHAR(255) NOT NULL,
     user_id INT NOT NULL,
     expiration_date TIMESTAMP NOT NULL,
-    PRIMARY KEY (token)
+    PRIMARY KEY (token),
     FOREIGN KEY (user_id) REFERENCES users(id) 
 );
